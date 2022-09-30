5c35df64d24fa8432e3f2c42fe0b4401d08e5635
Viktor <364185@protonmail.com>
2022-09-30 15:34:06 +0400

    int commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => {
+    return <header className='header'>
+        <img src='https://audimediacenter-a.akamaihd.net/system/production/media/1282/images/bde751ee18fe149036c6b47d7595f6784f8901f8/AL090142_full.jpg?1581961854'/>
+    </header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img src='https://assets.whichcar.com.au/image/private/s--0miyx05r--/c_fill,f_auto,q_auto:good/c_scale,w_1350/20_Years_Audi_RS6_Roadtrip_on_location_263.jpg'/>
+        </div>
+        <div>
+            Ava + description
+            <img src='https://pbs.twimg.com/media/FVja8U4UAAABcX0.jpg'/>
+        </div>
+        <div>
+            My posts
+            <div>
+                New post
+            </div>
+            <div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+                <div>
+                    post 3
+                </div>
+            </div>
+        </div>
+    </div>
+}
+export default Profile;
